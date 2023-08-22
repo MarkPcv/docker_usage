f865620aae8ffbb2fd772cb02da40c55947959b5
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-08-22 09:19:29 +0300

    Add testing for list of habits and validation on period and execution time

--- habit_tracker/tests.py
+++ habit_tracker/tests.py
@@ -5,6 +5,7 @@ from rest_framework.test import APITestCase
 from habit_tracker.models import Habit
 from users.models import User
 
+
 # print(response.json()) ## TODO remove after FINISH
 
 
@@ -12,6 +13,7 @@ class HabitTest(APITestCase):
     """
     Class for testing model `habit_tracker.Model`
     """
+
     def create_user(self):
         """Creates a new user for testing"""
         # Create test user with MEMBER role
@@ -165,10 +167,136 @@ class HabitTest(APITestCase):
             Habit.objects.exists()
         )
 
+    def test_habit_list(self):
+        """Testing list of habits"""
+        # Authenticate user without token
+        self.client.force_authenticate(self.user)
+        # Get the list of habits
+        response = self.client.get(
+            '/habits/'
+        )
+        # Check status
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+        # Check habit data
+        self.assertEqual(
+            response.json(),
+            {
+                "count": 1,
+                "next": None,
+                "previous": None,
+                "results": [{'id': self.habit.pk, 'place': 'place1',
+                             'action': 'action1',
+                             'time': '07:00:00', 'is_pleasant': False,
+                             'is_public': True,
+                             'exec_time': 60, 'period': 1, 'award': None,
+                             'associated_habit': None, 'owner': self.user.pk}]
+            }
+        )
+
     def test_habit_print(self):
         """Testing string representation of habit model"""
         self.assertEqual(
             self.habit.__str__(),
             'I will action1 at 07:00 in place1'
         )
+
+    def test_validate_period_exceeds(self):
+        """Testing period validation case when it is greater than 7 days"""
+        # Authenticate user without token
+        self.client.force_authenticate(self.user)
+        # Update action of habit
+        data = {
+            'period': 10
+        }
+        # Change first habit
+        response = self.client.patch(
+            f'/habits/{self.habit.pk}/',
+            data=data
+        )
+        # Check validation case
+        self.assertEqual(
+            response.json(),
+            {
+                "period": [
+                    "The period must be less or equal than 7 days"
+                ]
+            }
+        )
+
+    def test_validate_period_zero(self):
+        """Testing period validation when it is a zero"""
+        # Authenticate user without token
+        self.client.force_authenticate(self.user)
+        # Update action of habit
+        data = {
+            'period': 0
+        }
+        # Change first habit
+        response = self.client.patch(
+            f'/habits/{self.habit.pk}/',
+            data=data
+        )
+        # Check validation case
+        self.assertEqual(
+            response.json(),
+            {
+                "period": [
+                    "The period must be a non zero number"
+                ]
+            }
+        )
+
+    def test_validate_exec_time_exceeds(self):
+        """
+        Testing execution time validation case when it exceeds 120 seconds
+        """
+        # Authenticate user without token
+        self.client.force_authenticate(self.user)
+        # Update action of habit
+        data = {
+            'exec_time': 200
+        }
+        # Change first habit
+        response = self.client.patch(
+            f'/habits/{self.habit.pk}/',
+            data=data
+        )
+        # Check validation case
+        self.assertEqual(
+            response.json(),
+            {
+                "exec_time": [
+                    "The execution time cannot exceed 120 seconds"
+                ]
+            }
+        )
+
+    def test_validate_exec_time_zero(self):
+        """
+        Testing execution time validation case when it is a zero
+        """
+        # Authenticate user without token
+        self.client.force_authenticate(self.user)
+        # Update action of habit
+        data = {
+            'exec_time': 0
+        }
+        # Change first habit
+        response = self.client.patch(
+            f'/habits/{self.habit.pk}/',
+            data=data
+        )
+        # Check validation case
+        self.assertEqual(
+            response.json(),
+            {
+                "exec_time": [
+                    "The execution time must be a non zero number"
+                ]
+            }
+        )
+
 
