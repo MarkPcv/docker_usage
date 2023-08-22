cab1bd14f95dc9ab5be705f09932f88da35fe499
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-08-22 10:20:31 +0300

    Add Redis broker settings for Celery

--- config/settings.py
+++ config/settings.py
@@ -151,3 +151,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+# Broker settings for Celery
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
